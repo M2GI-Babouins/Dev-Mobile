69849e649294ffd2c9e3e88a88660e89f04e69ba
Morgane Henrion <morgane.henrion@skopai.com>
2022-02-16 14:24:26 +0100

    add playlist

--- src/app/modals/create-playlist/create-playlist.component.ts
+++ src/app/modals/create-playlist/create-playlist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
 import { Playlist } from 'src/app/models/playlist';
@@ -12,16 +13,20 @@ import { PlaylistService } from 'src/app/services/playlist.service';
 export class CreatePlaylistComponent implements OnInit {
 
   playlistForm: FormGroup
+  playlistsCollection : AngularFirestoreCollection<Playlist>
 
   constructor(private fb: FormBuilder, private playlistService: PlaylistService,
-    private modalController: ModalController) {
+    private modalController: ModalController,
+    private afs: AngularFirestore) {
+    this.playlistsCollection = this.afs.collection<Playlist>('playlists');
     this.playlistForm = this.fb.group({ name: ['', [Validators.required, Validators.minLength(3)]] });
   }
 
   ngOnInit() { }
 
   addPlaylist() {
-    this.playlistService.addPlaylist(new Playlist(this.playlistForm.get('name').value));
+    const newPlayList: Playlist = {name: this.playlistForm.get('name').value};
+    this.playlistsCollection.add(newPlayList);
     this.modalController.dismiss();
   }
 
--- src/app/models/playlist.ts
+++ src/app/models/playlist.ts
@@ -1,9 +1,9 @@
 import { Todo } from "./todo";
 
 export class Playlist {
-    id: number;
+    id?: number;
     name: string;
-    todos: Todo[];
+    todos?: Todo[];
 
     constructor(name: string, id?: number, todos?: Todo[]) {
         this.id = id ?? Math.floor(Math.random() * 100) + Date.now();
